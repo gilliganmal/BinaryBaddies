918341f6042ecb3276eb64886a08261a8993a9b9
gilliganmal <gilligan.mal@northeastern.edu>
2024-04-07 17:06:14 +0000

    updagte

--- C2_Client/client_pb2.py
+++ C2_Client/client_pb2.py
@@ -3,6 +3,7 @@
 # source: client.proto
 # Protobuf Python Version: 5.26.1
 """Generated protocol buffer code."""
+import google.protobuf
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import symbol_database as _symbol_database
