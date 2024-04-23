b9819c123f226f1bacf825be1b8c9d1baca387c1
aishuvinod <aishwarya2003vinod@gmail.com>
2024-04-23 21:18:51 +0000

    test

--- cvnt/cvnt/testservercrypto.py
+++ cvnt/cvnt/testservercrypto.py
@@ -1,11 +1,15 @@
+import nacl.public
 import nacl.secret
 import nacl.utils
 import base64
-from nacl.public import PrivateKey
+from nacl.public import PrivateKey, SealedBox
 
 # Generate public private key pair
 server_private_key = PrivateKey.generate()
-server_public_key = server_private_key.public_key
+#server_public_key = server_private_key.public_key
+#print(server_public_key.encode(encoder=nacl.encoding.HexEncoder).decode('utf-8'))
+server_public_key = b'b67aaeb539c0b7dc0d2c68a0a5b34ff4f24758c424875cdfd9d985e9ab1d1d73'
+
 
 # Load the ciphertext from the file
 with open("ciphertext.py", "r") as file:
@@ -15,7 +19,7 @@ with open("ciphertext.py", "r") as file:
 ciphertext_bytes = base64.b64decode(ciphertext)
 
 # Decrypt the ciphertext
-plaintext = nacl.secret.SealedBox(server_private_key).decrypt(ciphertext_bytes)
+plaintext = nacl.public.SealedBox(server_private_key).decrypt(ciphertext_bytes)
 
 # Print the decrypted plaintext
 print("Decrypted plaintext:", plaintext.decode('utf-8'))
