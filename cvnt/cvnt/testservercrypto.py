5956cea2deab79598f080624a55a35c3d2e68201
aishuvinod <aishwarya2003vinod@gmail.com>
2024-04-23 20:52:27 +0000

    test

--- cvnt/cvnt/testservercrypto.py
+++ cvnt/cvnt/testservercrypto.py
@@ -6,9 +6,6 @@ from nacl.public import PrivateKey
 # Generate public private key pair
 server_private_key = PrivateKey.generate()
 server_public_key = server_private_key.public_key
-# Encode the public key in hexadecimal format for easy embedding or display
-encoded_public_key = server_public_key.encode(encoder=HexEncoder).decode('utf-8')
-print(encoded_public_key)
 
 # Load the ciphertext from the file
 with open("ciphertext.py", "r") as file:
