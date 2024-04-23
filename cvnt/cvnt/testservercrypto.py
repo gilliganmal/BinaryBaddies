68472b34d404d30fa8ea7a82d323d682448f25a6
aishuvinod <aishwarya2003vinod@gmail.com>
2024-04-23 21:27:56 +0000

    test

--- cvnt/cvnt/testservercrypto.py
+++ cvnt/cvnt/testservercrypto.py
@@ -4,12 +4,28 @@ import nacl.utils
 import base64
 from nacl.public import PrivateKey, SealedBox
 
+
 # Generate public private key pair
 server_private_key = PrivateKey.generate()
-#server_public_key = server_private_key.public_key
-#print(server_public_key.encode(encoder=nacl.encoding.HexEncoder).decode('utf-8'))
-server_public_key = b'b67aaeb539c0b7dc0d2c68a0a5b34ff4f24758c424875cdfd9d985e9ab1d1d73'
+server_public_key = server_private_key.public_key
+print(server_public_key.encode(encoder=nacl.encoding.HexEncoder).decode('utf-8'))
 
+def generate_or_load_private_key(filename):
+    try:
+        # Try to load the private key from file
+        with open(filename, "rb") as file:
+            private_key = PrivateKey(file.read())
+        print("Private key loaded successfully")
+    except FileNotFoundError:
+        # If file doesn't exist, generate a new private key and save it
+        private_key = PrivateKey.generate()
+        with open(filename, "wb") as file:
+            file.write(private_key.encode())
+        print("New private key generated and saved")
+    return private_key
+
+# Generate or load the private key
+server_private_key = generate_or_load_private_key("private_key.bin")
 
 # Load the ciphertext from the file
 with open("ciphertext.py", "r") as file:
@@ -17,11 +33,14 @@ with open("ciphertext.py", "r") as file:
 
 # Decode the Base64-encoded ciphertext
 ciphertext_bytes = base64.b64decode(ciphertext)
-print(ciphertext, "this is ciphertext")
-print(ciphertext_bytes, "ciphertextbytes")
+
 # Decrypt the ciphertext
 plaintext = nacl.public.SealedBox(server_private_key).decrypt(ciphertext_bytes)
 
 # Print the decrypted plaintext
 print("Decrypted plaintext:", plaintext.decode('utf-8'))
+
+
+
+
 
